15846596397f81671bce7a45ee8f14a18388a2c7
RozhinaNadya <zhuk.09@mail.ru>
2021-10-12 16:50:35 +0300

    создали файл ProfileView.swift и добавили @IBOoutlet'ы для всех элементов

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Надежда on 12.10.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var avatarImage: UIImageView!
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var dateOfBirthLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
